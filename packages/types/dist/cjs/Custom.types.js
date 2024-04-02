798652050a9d39c1996dfbac6bff1d3cada8bbae
Oscar Rene Cuellar Mancilla <121446099+KokaJS@users.noreply.github.com>
2024-04-02 12:52:28 -0500

    # types ES & Common

---
+++ packages/types/dist/cjs/Custom.types.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UNIDADES_DE_MEDIDA = exports.MONTHS = void 0;
+exports.MONTHS = {
+    ene: 0,
+    feb: 1,
+    mar: 2,
+    abr: 3,
+    may: 4,
+    jun: 5,
+    jul: 6,
+    ago: 7,
+    sep: 8,
+    oct: 9,
+    nov: 10,
+    dic: 11,
+};
+exports.UNIDADES_DE_MEDIDA = [
+    { label: "Frasco", value: "frasco", stocks_only_integer: false },
+    { label: "Bolsa", value: "bolsa", stocks_only_integer: false },
+    { label: "Paquete", value: "paquete", stocks_only_integer: false },
+    { label: "Caja", value: "caja", stocks_only_integer: false },
+    { label: "Unidades", value: "unidades", stocks_only_integer: true },
+];
