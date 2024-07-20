ed00b1ec7ab2954ea132e8470a8f18dc9f9dbb00
Oscar Cuellar <oscar@zauru.com>
2024-07-19 21:16:53 -0500

    with webapptables

--- packages/graphql/src/GraphQL.queries.ts
+++ packages/graphql/src/GraphQL.queries.ts
@@ -334,6 +334,11 @@ export const getPurchaseOrdersBetweenDatesStringQuery = (
           description
           ${lotStocksFragment}
         }
+        webapp_table_rowables {
+            webapp_rows {
+                data
+            }
+        }
         shipment_purchase_orders {
           shipment {
             id
