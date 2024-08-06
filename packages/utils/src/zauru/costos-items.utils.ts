f63add2b94c88b2fc424da272f0603af932f0fdc
Oscar Cuellar <oscar@zauru.com>
2024-08-06 13:14:18 -0500

    Update costos-items.utils.ts

--- packages/utils/src/zauru/costos-items.utils.ts
+++ packages/utils/src/zauru/costos-items.utils.ts
@@ -118,9 +118,9 @@ export const obtenerSemanaAnterior = () => {
     " al Domingo " +
     fechaFin.date() +
     ", de " +
-    fechaInicio.format("MMMM") +
+    fechaFin.format("MMMM") +
     " de " +
-    fechaInicio.year();
+    fechaFin.year();
 
   return {
     fechaInicio: fechaInicioFormateada,
