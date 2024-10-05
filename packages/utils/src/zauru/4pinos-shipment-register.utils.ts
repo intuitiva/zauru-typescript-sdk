524f2c0030eae2249ea5dcd33ecd2d68621510f9
Oscar Cuellar <oscar@zauru.com>
2024-10-04 20:20:19 -0500

    # Retry shipment process

--- packages/utils/src/zauru/4pinos-shipment-register.utils.ts
+++ packages/utils/src/zauru/4pinos-shipment-register.utils.ts
@@ -1,4 +1,9 @@
+import { Session } from "@remix-run/node";
 import { getHeaders, getSession } from "@zauru-sdk/services";
+import { QueueShipmentsForm } from "@zauru-sdk/types";
+import { WebAppRowGraphQL } from "@zauru-sdk/types";
+import { updateQueueShipmentsFormHistory } from "./4pinos-shipments-form-history.utils.js";
+import { ESTADOS_COLA_RECEPCIONES } from "./4pinos-receptions-form-history.utils.js";
 
 export const register4pinosShipment = async ({
   cookie,
@@ -22,4 +27,70 @@ export const register4pinosShipment = async ({
   const headers = await getHeaders(cookie, session);
 
   console.log(values);
+};
+
+export const retryShipmennt = async (
+  register: WebAppRowGraphQL<QueueShipmentsForm>,
+  session: Session,
+  headers: any,
+  hostname: string,
+  cookie: string | null
+) => {
+  console.log("REINTENTO DE REGISTRO DE ENVÍO =======================>");
+
+  const queueResponse = await updateQueueShipmentsFormHistory(
+    headers,
+    session,
+    {
+      estado: ESTADOS_COLA_RECEPCIONES.REINTENTANDO,
+    },
+    register.id
+  );
+
+  if (queueResponse.error || !queueResponse.data?.id) {
+    return {
+      error: true,
+      title: "No se logró comunicar con la red.",
+      type: "error",
+      description: `No hay comunicación con la red para el reintento: ${queueResponse?.userMsg}`,
+    };
+  }
+
+  if (hostname.includes("localhost")) {
+    console.log(
+      "------- EJECUTANDO LOCALMENTE",
+      ` para ${register.data?.formSubmited?.shipment_number}`
+    );
+    register4pinosShipment({
+      values: register.data?.formSubmited,
+      cookie: cookie || "",
+      idWebAppTable: register.id,
+      agency_id: register.data?.agency_id,
+    });
+  } else {
+    console.log(
+      "------- ENVIANDO A EJECUTAR NETLIFY FUNCTION: 4pinosNewShipment-background",
+      ` para ${register.data?.formSubmited?.shipment_number}`
+    );
+    fetch(
+      `https://${hostname}/.netlify/functions/4pinosNewShipment-background`,
+      {
+        method: "post",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({
+          values: register.data?.formSubmited,
+          cookie: cookie || "",
+          idWebAppTable: register.id,
+          agency_id: register.data?.agency_id,
+        }),
+      }
+    );
+  }
+
+  return {
+    error: false,
+    title: "Reintento iniciado con éxito.",
+    type: "success",
+    description: `Se está procesando el reintento de ingreso de recepción!.`,
+  };
 };
