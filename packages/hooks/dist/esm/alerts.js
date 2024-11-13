674af7c0eb3634f2aff909e0b34d6cff38016ba5
Oscar Cuellar <oscar@zauru.com>
2024-11-13 09:46:49 -0600

    # update alert

--- packages/hooks/dist/esm/alerts.js
+++ packages/hooks/dist/esm/alerts.js
@@ -5,33 +5,39 @@ const react_1 = require("react");
 const index_js_1 = require("./index.js");
 const useValidateNotifications = (source) => {
     const { actionData, fetcher, loaderData } = source;
+    const [alertShown, setAlertShown] = (0, react_1.useState)({});
     (0, react_1.useEffect)(() => {
-        if (loaderData?.title) {
+        if (loaderData?.title && !alertShown[loaderData.title]) {
             (0, index_js_1.showAlert)({
                 description: loaderData?.description?.toString() ?? "",
                 title: loaderData?.title,
                 type: loaderData?.type,
             });
+            setAlertShown({ ...alertShown, [loaderData.title]: true });
         }
-    }, [loaderData]);
+    }, [loaderData, alertShown]);
     (0, react_1.useEffect)(() => {
-        if (fetcher?.data?.title && fetcher.state === "idle") {
+        if (fetcher?.data?.title &&
+            fetcher.state === "idle" &&
+            !alertShown[fetcher.data.title]) {
             (0, index_js_1.showAlert)({
                 description: fetcher.data?.description,
                 title: fetcher.data?.title,
                 type: fetcher.data?.type,
             });
+            setAlertShown({ ...alertShown, [fetcher.data.title]: true });
         }
-    }, [fetcher?.data, fetcher?.state]);
+    }, [fetcher?.data, fetcher?.state, alertShown]);
     (0, react_1.useEffect)(() => {
-        if (actionData?.title) {
+        if (actionData?.title && !alertShown[actionData.title]) {
             (0, index_js_1.showAlert)({
                 description: actionData?.description ?? "",
                 title: actionData?.title,
                 type: actionData?.type,
             });
+            setAlertShown({ ...alertShown, [actionData.title]: true });
         }
-    }, [actionData]);
+    }, [actionData, alertShown]);
     return null;
 };
 exports.useValidateNotifications = useValidateNotifications;
--- packages/hooks/src/alerts.ts
+++ packages/hooks/src/alerts.ts
@@ -1,5 +1,5 @@
 import type { FetcherWithComponents } from "@remix-run/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { showAlert } from "./index.js";
 
 export const useValidateNotifications = (source: {
@@ -8,36 +8,44 @@ export const useValidateNotifications = (source: {
   loaderData?: any;
 }) => {
   const { actionData, fetcher, loaderData } = source;
+  const [alertShown, setAlertShown] = useState({});
 
   useEffect(() => {
-    if (loaderData?.title) {
+    if (loaderData?.title && !alertShown[loaderData.title]) {
       showAlert({
         description: loaderData?.description?.toString() ?? "",
         title: loaderData?.title,
         type: loaderData?.type as any,
       });
+      setAlertShown({ ...alertShown, [loaderData.title]: true });
     }
-  }, [loaderData]);
+  }, [loaderData, alertShown]);
 
   useEffect(() => {
-    if (fetcher?.data?.title && fetcher.state === "idle") {
+    if (
+      fetcher?.data?.title &&
+      fetcher.state === "idle" &&
+      !alertShown[fetcher.data.title]
+    ) {
       showAlert({
         description: fetcher.data?.description,
         title: fetcher.data?.title,
         type: fetcher.data?.type,
       });
+      setAlertShown({ ...alertShown, [fetcher.data.title]: true });
     }
-  }, [fetcher?.data, fetcher?.state]);
+  }, [fetcher?.data, fetcher?.state, alertShown]);
 
   useEffect(() => {
-    if (actionData?.title) {
+    if (actionData?.title && !alertShown[actionData.title]) {
       showAlert({
         description: actionData?.description ?? "",
         title: actionData?.title,
         type: actionData?.type as any,
       });
+      setAlertShown({ ...alertShown, [actionData.title]: true });
     }
-  }, [actionData]);
+  }, [actionData, alertShown]);
 
   return null;
 };
