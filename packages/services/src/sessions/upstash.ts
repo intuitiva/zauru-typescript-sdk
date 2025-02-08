5ac24dac67ee1da2a8c8809b4f56bb1ca584e400
Oscar Cuellar <oscar@zauru.com>
2025-02-08 00:16:42 -0600

    Revert "# Nuevo formateo de ID session"
    This reverts commit 29b9f13b74d0bd21a76bf345a42517dce466eba7.

--- packages/services/src/sessions/upstash.ts
+++ packages/services/src/sessions/upstash.ts
@@ -53,9 +53,7 @@ export function createUpstashSessionStorage({ cookie }: any) {
   return createSessionStorage({
     cookie,
     async createData(data, expires) {
-      const id: string = `${data?.selectedEntity}-${
-        data?.employee_id
-      }-${crypto.randomUUID()}`;
+      const id: string = `$${crypto.randomUUID()}`;
       await fetchWithRetries(
         `${redisBaseURL}/set/${id}?EX=${
           expires ? expiresToSeconds(expires) : 60 * 60 * 8
