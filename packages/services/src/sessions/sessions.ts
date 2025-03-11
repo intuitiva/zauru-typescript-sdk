e6f085b39a7e10d38f9aa50d2d5beeb6c2423c0e
Oscar Cuellar <oscarrenecuellarmancilla@gmail.com>
2025-03-11 11:43:44 -0500

    Update sessions.ts

--- packages/services/src/sessions/sessions.ts
+++ packages/services/src/sessions/sessions.ts
@@ -1,4 +1,4 @@
-import { createCookie } from "@remix-run/node";
+import { createCookie, Session } from "@remix-run/node";
 import { createUpstashSessionStorage } from "./upstash.js";
 
 const sessionCookie = createCookie("_rj_session", {
@@ -14,4 +14,21 @@ const sessionCookie = createCookie("_rj_session", {
 const { getSession, commitSession, destroySession } =
   createUpstashSessionStorage({ cookie: sessionCookie });
 
-export { getSession, commitSession, destroySession };
+const getRefreshSession = async (request: Request, session: Session) => {
+  const cookie = request.headers.get("Cookie");
+  const currentSession = await getSession(cookie);
+
+  if (!currentSession) {
+    return null;
+  }
+
+  return await commitSession(session, {
+    maxAge: 60 * 60 * 24,
+    path: "/",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  });
+};
+
+export { getSession, commitSession, destroySession, getRefreshSession };
